3041c8208c28aa61e341def3a4d295a10bb5fdda
rabolton <rob.a.bolton@gmail.com>
2013-11-20 11:27:18 +0000

    Updated IWalkInterface to use IPointOfInterest. IWalkInterface imports uk.aber.group14.model*;

--- SRC/Drafts/src/uk/ac/aber/group14/controller/IWalkInterface.java
+++ SRC/Drafts/src/uk/ac/aber/group14/controller/IWalkInterface.java
@@ -1,7 +1,8 @@
 package uk.ac.aber.group14.controller;
+import uk.ac.aber.group14.model.*;
 
 public interface IWalkInterface {
 	
-	public addPOI(PointOfInterest point);
+	public void addPOI(IPointOfInterest point);
 	public void cancelWalk();
 }
