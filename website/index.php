7cf3d09c6a1fa8773eef971ae5c4f11932425c86
Jake Maguire <jam64@lnx-c56-11.dcs.aber.ac.uk>
2013-11-22 14:15:47 +0000

    update

--- website/index.php
+++ website/index.php
@@ -29,14 +29,26 @@
 				mysql_select_db('walking_tour_database',$connection) or die("cannont connect to database");
 
 				/* show tables */
-				$result = mysql_query($connection,"SELECT * FROM listOfWalks");
-				while($row = mysql_fetch_array($result))
-				{
-					echo $row['id'] . " " . $row['title'];
-					echo "<br>";
-				}
+				$result = mysqli_query($con,"SELECT * FROM listOfWalks");
+				while($tableName = mysql_fetch_row($result)) {
 
-				mysql_close($connection);
+					$table = $tableName[0];
+	
+					echo '<h3>',$table,'</h3>';
+					$result2 = mysql_query('SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
+					if(mysql_num_rows($result2)) {
+						echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+						echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
+						while($row2 = mysql_fetch_row($result2)) {
+							echo '<tr>';
+							foreach($row2 as $key=>$value) {
+								echo '<td>',$value,'</td>';
+							}
+							echo '</tr>';
+						}
+						echo '</table><br />';
+					}
+				}
 				
 
 					/*mysql_query($dbhandle, "INSERT INTO listOfWalks (title) VALUES ('test')");
