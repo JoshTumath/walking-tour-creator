7e0efdbad22a3fe989b8da19f8eede371125db63
Jake Maguire <jam64@lnx-c56-11.dcs.aber.ac.uk>
2013-11-22 14:26:42 +0000

    update

--- website/index.php
+++ website/index.php
@@ -39,31 +39,6 @@
 					print $db_field['longDesc'] . "<BR>";
 
 				}
-				
-				/* show tables */
- 				$result = mysql_query('SHOW TABLES',$connection) or die('cannot show tables');
-				while($tableName = mysql_fetch_row($result)) {
-
-					$table = $tableName[0];
-	
-					echo '<h3>',$table,'</h3>';
-					$result2 = mysql_query('SHOW COLUMNS FROM '.$table) or die('cannot show columns from '.$table);
-					if(mysql_num_rows($result2)) {
-						echo '<table cellpadding="0" cellspacing="0" class="db-table">';
-						echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
-						while($row2 = mysql_fetch_row($result2)) {
-							echo '<tr>';
-							foreach($row2 as $key=>$value) {
-								echo '<td>',$value,'</td>';
-							}
-							echo '</tr>';
-						}
-						echo '</table><br />';
-					}
-				}
-
-
-				
 
 					/*mysql_query($dbhandle, "INSERT INTO listOfWalks (title) VALUES ('test')");
 						
