42e95ee06842f2ad79db8e445001a1f81bbff6f2
Robert A Bolton <rob.a.bolton@gmail.com>
2013-12-13 09:10:08 +0000

    Added IWalk interface, fleshed out IPointOfInterest interface.

---
+++ SRC/Drafts/src/uk/ac/aber/group14/controller/IWalkController.java
@@ -0,0 +1,9 @@
+package uk.ac.aber.group14.controller;
+import uk.ac.aber.group14.model.*;
+
+public interface IWalkController {
+	
+	public void addPOI(IPointOfInterest point);
+	
+	public void cancelWalk();
+}
