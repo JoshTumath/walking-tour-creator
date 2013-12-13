7e5cf7b2f45d8242ce1a12d9a10472218e9ddc42
Robert A Bolton <rob.a.bolton@gmail.com>
2013-12-13 08:22:27 +0000

    Added walk details activity

--- SRC/Drafts/gen/uk/ac/aber/group14/R.java
+++ SRC/Drafts/gen/uk/ac/aber/group14/R.java
@@ -25,24 +25,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int NewWalk=0x7f080001;
-        public static final int action_settings=0x7f080009;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080006;
-        public static final int gridLayout1=0x7f080000;
-        public static final int spinner1=0x7f080003;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f080008;
-        public static final int textView4=0x7f080007;
+        public static final int Frame_Layout_Main=0x7f080001;
+        public static final int NewWalk=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int button3=0x7f080007;
+        public static final int gridLayout1=0x7f080002;
+        public static final int spinner1=0x7f080004;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080009;
+        public static final int textView4=0x7f080008;
+        public static final int walkDetailsCancelButton=0x7f080003;
+        public static final int walkDetailsConfirmButton=0x7f08000f;
+        public static final int walkDetailsLDEdit=0x7f08000e;
+        public static final int walkDetailsLDLabel=0x7f08000d;
+        public static final int walkDetailsNameEdit=0x7f08000c;
+        public static final int walkDetailsNameLabel=0x7f080005;
+        public static final int walkDetailsSDEdit=0x7f08000a;
+        public static final int walkDetailsSDLabel=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main_app=0x7f030000;
         public static final int activity_walk_creator=0x7f030001;
+        public static final int activity_walk_details=0x7f030002;
     }
     public static final class menu {
         public static final int main_app=0x7f070000;
         public static final int walk_creator=0x7f070001;
+        public static final int walk_details=0x7f070002;
     }
     public static final class string {
         public static final int AddButton=0x7f050005;
@@ -54,11 +63,20 @@ public final class R {
         public static final int Main_CreateWalk_Button=0x7f05000d;
         public static final int SaveButton=0x7f050004;
         public static final int WalkCreatorTitle=0x7f050006;
+        public static final int WalkDetailsLDLabel=0x7f050013;
+        public static final int WalkDetailsNameLDHint=0x7f050014;
+        public static final int WalkDetailsNameLabel=0x7f05000f;
+        public static final int WalkDetailsNameSDHint=0x7f050012;
+        public static final int WalkDetailsNameTextHint=0x7f050010;
+        public static final int WalkDetailsSDLabel=0x7f050011;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f05000c;
         public static final int title_activity_main_app=0x7f05000b;
         public static final int title_activity_walk_creator=0x7f050001;
+        public static final int title_activity_walk_details=0x7f05000e;
+        public static final int walkDetailsCancelButtonText=0x7f050015;
+        public static final int walkDetailsConfirmButtonText=0x7f050016;
     }
     public static final class style {
         /** 
--- SRC/Drafts/src/uk/ac/aber/group14/viewer/WalkDetailsActivity.java
+++ SRC/Drafts/src/uk/ac/aber/group14/viewer/WalkDetailsActivity.java
@@ -1,21 +1,22 @@
-package uk.ac.aber.group14;
+package uk.ac.aber.group14.viewer;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import uk.ac.aber.group14.R;
 
-public class MainAppActivity extends Activity {
+public class WalkDetailsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_app);
+		setContentView(R.layout.activity_walk_details);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_app, menu);
+		getMenuInflater().inflate(R.menu.walk_details, menu);
 		return true;
 	}
 
