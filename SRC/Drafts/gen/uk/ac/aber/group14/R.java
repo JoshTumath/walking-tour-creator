ac148ce831717881fc1feb76cc19c9da9fe3dd71
unknown <meo9@PSVWS099.pau.local>
2014-01-27 15:07:26 +0000

    previous commit was a mistake

--- SRC/Drafts/gen/uk/ac/aber/group14/R.java
+++ SRC/Drafts/gen/uk/ac/aber/group14/R.java
@@ -29,7 +29,7 @@ public final class R {
         public static final int NewWalk=0x7f080008;
         public static final int action_settings=0x7f080017;
         public static final int addPicture=0x7f080007;
-        public static final int button3=0x7f08000d;
+        public static final int button3=0x7f08000c;
         public static final int dialogButtonOK=0x7f080016;
         public static final int digitalClock1=0x7f080001;
         public static final int editText1=0x7f080003;
@@ -46,7 +46,7 @@ public final class R {
         public static final int walkDetailsLDEdit=0x7f080014;
         public static final int walkDetailsLDLabel=0x7f080012;
         public static final int walkDetailsNameEdit=0x7f080011;
-        public static final int walkDetailsNameLabel=0x7f08000c;
+        public static final int walkDetailsNameLabel=0x7f08000d;
         public static final int walkDetailsSDEdit=0x7f08000f;
         public static final int walkDetailsSDLabel=0x7f080010;
     }
