0da114b6ee27afe5d8ee1129f2d0eb1b6e467a58
Josh Tumath <josh@joshtumath.me.uk>
2014-01-28 11:44:53 +0000

    Corrected name in header

--- SRC/Drafts/website/includes/header_bottom.php
+++ SRC/Drafts/website/includes/header_bottom.php
@@ -1,6 +1,6 @@
 	</head>	
 	<body <?php if (!is_null($body_class)) : echo 'class="' . $body_class . '"'; endif; ?>>
 		<header>
-			<h1><a href="index.php">Walking Tour Creator</a></h1>
+			<h1><a href="index.php">Walking Tour Displayer</a></h1>
 		</header>
 		<main>
