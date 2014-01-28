1500e472aab7c305c51b32b34ea44fa090d3c359
Josh Tumath <josh@joshtumath.me.uk>
2014-01-28 11:09:29 +0000

    Forgot to add class="" for body element

--- SRC/Drafts/website/includes/header_bottom.php
+++ SRC/Drafts/website/includes/header_bottom.php
@@ -1,5 +1,5 @@
 	</head>	
-	<body <?php if (!is_null($body_class)) : echo $body_class; endif; ?>>
+	<body <?php if (!is_null($body_class)) : echo 'class="' . $body_class . '"'; endif; ?>>
 		<header>
 			<h1><a href="index.php">Walking Tour Creator</a></h1>
 		</header>
