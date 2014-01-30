5a499258b0f98984195aaaf78ed72a5d627b44f0
JamesMellors <jayt.v@hotmail.com>
2014-01-30 17:31:43 +0000

    Just messing around with the tests

--- SRC/Drafts/src/uk/ac/aber/group14/Junit/TestWalk.java
+++ SRC/Drafts/src/uk/ac/aber/group14/Junit/TestWalk.java
@@ -1,7 +1,38 @@
 package uk.ac.aber.group14.Junit;
 
+import java.util.LinkedList;
+
+import uk.ac.aber.group14.model.IPointOfInterest;
+import uk.ac.aber.group14.model.Walk;
+import android.location.Location;
 import junit.framework.TestCase;
 
 public class TestWalk extends TestCase {
+	
+	public TestWalk(String name, String shortDescription, String longDescription) {
+		super();
+		
+	}
 
+	private String name;
+	private String shortDescription;
+	private String longDescription;
+	private LinkedList<IPointOfInterest> points;
+	private LinkedList<Location> locations;
+	
+	public void setUp(){
+		name="walk";
+		shortDescription="short walk Test";
+		longDescription="long walk Test";
+		
+	}
+	
+	public void TestWalk(){
+		String result="walk";
+	}
+
+	public void TestgetLongDescription(){
+	
+	//	Walk.get;
+	}
 }
