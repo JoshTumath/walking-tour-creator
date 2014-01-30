186d349c98f2531e0d0669655283e670ec157b02
rabolton <rob.a.bolton@gmail.com>
2014-01-30 17:55:36 +0000

    Added javadoc to IJsonPackager

--- SRC/Drafts/src/uk/ac/aber/group14/model/IJsonPackager.java
+++ SRC/Drafts/src/uk/ac/aber/group14/model/IJsonPackager.java
@@ -1,7 +1,17 @@
 package uk.ac.aber.group14.model;
 
+/**
+ * This interface is used to convert an IWalk to a JSON String
+ * @author Group14
+ *
+ */
 public interface IJsonPackager {
 	
+	/**
+	 * This takes an IWalk and converts it into a JSON String
+	 * @param w The walk which will be converted
+	 * @return A String containing the walk represented as a JSON object
+	 */
 	public String JSONify(IWalk w);
 	
 }
