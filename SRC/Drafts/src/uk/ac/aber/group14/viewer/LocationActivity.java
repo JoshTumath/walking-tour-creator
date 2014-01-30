4fe340cf91c8f131b3e9b6703ffac6017b5273bc
rabolton <rob.a.bolton@gmail.com>
2014-01-30 15:10:54 +0000

    Added javadoc to the LocationActivity

--- SRC/Drafts/src/uk/ac/aber/group14/viewer/LocationActivity.java
+++ SRC/Drafts/src/uk/ac/aber/group14/viewer/LocationActivity.java
@@ -22,6 +22,10 @@ import android.widget.TextView;
 
 
 /**
+ * This activity is shown to the user when they add a new
+ * point of interest. It allows them to enter a name, description,
+ * and optionally take a picture with the camera intent.
+ * The information is added to an Intent as the activity finishes. 
  * 
  * @author Group14
  *
@@ -34,6 +38,14 @@ public class LocationActivity extends Activity {
 	static final int REQUEST_IMAGE_CAPTURE = 1;
 	static final String changePictureText = "Change picture";
 	
+	/* (non-Javadoc)
+	 * This method is overriden to load any data from the
+	 * savedInstanceState which was saved on a configuration change.
+	 * This allows us to re-populate the text boxes and use the
+	 * pictue that was caputed by the user before the change.
+	 * s
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -59,6 +71,13 @@ public class LocationActivity extends Activity {
 		
 	}
 	
+	/**
+	 * This method is used to check whether or not the input entered by
+	 * the user is valid. This checks if both the name & description
+	 * are not empty.
+	 * 
+	 * @return boolean to show whether the input was valid (T) or not (F)
+	 */
 	public boolean validInput() {
 		String locationName = ((TextView) findViewById(R.id.locationNameEdit)).getText().toString();
 		String locationDesc = ((TextView) findViewById(R.id.locationDescriptionEdit)).getText().toString();
@@ -66,7 +85,14 @@ public class LocationActivity extends Activity {
 	}
 	
 	/*
-	 * This method should be called by the "Add location button"
+	 * This method is called by the "add location" button.
+	 * It checks if the input is valid and saves the data to
+	 * an Intent if true.
+	 * This data can be retrieved by the calling Intent.
+	 * The Acitivity then closes with "finish()", assuming the data
+	 * was valid.
+	 * 
+	 * @param view
 	 */
 	public void addLocation(View view) {//button add location in confirm
 		if(validInput()) {
@@ -101,12 +127,25 @@ public class LocationActivity extends Activity {
 		}
 	}
 	
+	/**
+	 * This method is called by the cancel button, closing
+	 * the Activity and returning an Activity.RESULT_CANCELED
+	 * result.
+	 * 
+	 * @param view
+	 */
 	public void cancel(View view) {
 		setResult(Activity.RESULT_CANCELED, new Intent());
 		finish();
 	}
 	
 
+	/**
+	 * This method is called by the "Add picture" button. It
+	 * starts the camera intent.
+	 * 
+	 * @param view
+	 */
 	public void onClickPictureButton(View view) {
 	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -114,6 +153,14 @@ public class LocationActivity extends Activity {
 	    }
 	}
 	
+	/* (non-Javadoc)
+	 * This method is overriden to listen for the camera Intent's result.
+	 * If we receive a RESULT_OK and the request code was
+	 * REQUEST_IMAGE_CAPTURE then we pass the url to getPath
+	 * and set the picture to the returned file path String
+	 * 
+	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 	    // Check which request we're responding to
@@ -124,6 +171,14 @@ public class LocationActivity extends Activity {
 	    }
 	}
 
+	/**
+	 * This method takes a Uri and obtains the file path to
+	 * the resource. It accesses the local MediaStore database,
+	 * using the provided uri.
+	 * The first entry is returned, giving us our file's path.
+	 * @param uri The Uri object to find the path of
+	 * @return The path to the resource in the filesystem
+	 */
 	public String getPath(Uri uri) {
 		String[] projection = { MediaStore.Images.Media.DATA }; // Use image media table
 		Cursor cursor = managedQuery(uri, projection, null, null, null); // Although deprecated since API 11, we're working with API 8+
@@ -135,7 +190,15 @@ public class LocationActivity extends Activity {
 		cursor.moveToFirst();
 		return cursor.getString(column_index);//Return the first entry, which will be our file
 	}
-	// sends infromtaion from location name/Description to a String 
+	
+	 
+	/* (non-Javadoc)
+	 * This method is overriden in order to save the text box
+	 * contents in the event of a configuration change (screen
+	 * re-orientation etc)
+	 * 
+	 * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
+	 */
 	@Override
 	public void onSaveInstanceState(Bundle out) {
 		String name = ((EditText) findViewById(R.id.locationNameEdit)).getText().toString();
