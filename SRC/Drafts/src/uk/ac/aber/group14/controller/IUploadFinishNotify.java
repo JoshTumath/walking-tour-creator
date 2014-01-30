f83a14f0c2ed9f0315567ed915f8aada60827a9e
rabolton <rob.a.bolton@gmail.com>
2014-01-30 16:13:09 +0000

    Added javadoc to WalkController and IWalkController.

--- SRC/Drafts/src/uk/ac/aber/group14/controller/IUploadFinishNotify.java
+++ SRC/Drafts/src/uk/ac/aber/group14/controller/IUploadFinishNotify.java
@@ -1,6 +1,18 @@
 package uk.ac.aber.group14.controller;
 
+/**
+ * This interface is used to allow our WalkUploader AsyncTask
+ * to talk back to the calling Activity as long as it implements
+ * this interface. This is in order to signify when the file has
+ * been uploaded and the activity can call "finish()".
+ * 
+ * @author Group14
+ *
+ */
 public interface IUploadFinishNotify {
 
+	/**
+	 * This should be used to tell our class that it is finished.
+	 */
 	public void setFinished();
 }
