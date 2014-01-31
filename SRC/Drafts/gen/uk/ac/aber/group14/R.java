2717d48caadf8c596f9cddafefece4b3469b0c6a
Pyro-Lappy <pyro-lappy@ubuntu.(none)>
2014-01-31 10:45:34 +0000

    removed unused layout, fixed activity_location, again

--- SRC/Drafts/gen/uk/ac/aber/group14/R.java
+++ SRC/Drafts/gen/uk/ac/aber/group14/R.java
@@ -27,16 +27,15 @@ public final class R {
         public static final int logo2=0x7f020002;
     }
     public static final class id {
-        public static final int ConfirmAction=0x7f080007;
+        public static final int ConfirmAction=0x7f080008;
         public static final int NewWalk=0x7f08000a;
         public static final int TextView01=0x7f08000e;
-        public static final int action_settings=0x7f08001c;
-        public static final int addPicture=0x7f080008;
+        public static final int action_settings=0x7f08001a;
+        public static final int addPicture=0x7f080009;
         public static final int button3=0x7f080012;
-        public static final int dialogButtonOK=0x7f08001b;
         public static final int digitalClock1=0x7f080001;
         public static final int gallery2=0x7f08000c;
-        public static final int imageGallery=0x7f080009;
+        public static final int imageGallery=0x7f080007;
         public static final int imageView1=0x7f08000b;
         public static final int latitude=0x7f08000d;
         public static final int locationDescriptionEdit=0x7f080006;
@@ -44,7 +43,6 @@ public final class R {
         public static final int longitude=0x7f08000f;
         public static final int scrollView1=0x7f080000;
         public static final int spacer=0x7f080003;
-        public static final int text=0x7f08001a;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080010;
         public static final int textView3=0x7f080005;
@@ -62,7 +60,6 @@ public final class R {
         public static final int activity_main_app=0x7f030001;
         public static final int activity_walk_creator=0x7f030002;
         public static final int activity_walk_details=0x7f030003;
-        public static final int unimplemented=0x7f030004;
     }
     public static final class menu {
         public static final int location=0x7f070000;
